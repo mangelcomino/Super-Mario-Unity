fac72b1bb4e0d93f140c649593c3f71c82dda620
mangelcomino <mangelcomino@gmail.com>
2023-06-14 13:27:42 +0200

    iniciando movimiento horizontal

--- SuperMario/Assets/Scripts/Mario.cs
+++ SuperMario/Assets/Scripts/Mario.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.Rendering.VirtualTexturing;
 
@@ -7,11 +8,15 @@ public class Mario : MonoBehaviour
 {
 
     private Rigidbody2D mRigidBody;
+    private Animator mAnimator;
+    private SpriteRenderer mSpriteRenderer;
     public float speedX = 5f;
 
     private void Awake()
     {
         mRigidBody = GetComponent<Rigidbody2D>();
+        mAnimator = GetComponent<Animator>();
+        mSpriteRenderer = GetComponent<SpriteRenderer>();
     }
     // Start is called before the first frame update
     void Start()
@@ -25,5 +30,11 @@ public class Mario : MonoBehaviour
         float moveX = Input.GetAxis("Horizontal");
         Vector2 movement = new Vector2(moveX * speedX, mRigidBody.velocity.y);
         mRigidBody.velocity = movement;
+        if (moveX != 0)
+            mAnimator.Play("MarioRun");
+        else mAnimator.Play("MarioIdle");
+        if (moveX<0)
+            mSpriteRenderer.flipX = true;
+        else mSpriteRenderer.flipX = false;
     }
 }
