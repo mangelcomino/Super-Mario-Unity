6f3383de133e220238c6707a83a7e94b130ec49f
mangelcomino <mangelcomino@gmail.com>
2023-06-14 13:14:12 +0200

    movimiento inicial

--- SuperMario/Assets/Scripts/Mario.cs
+++ SuperMario/Assets/Scripts/Mario.cs
@@ -1,9 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.VirtualTexturing;
 
 public class Mario : MonoBehaviour
 {
+
+    private Rigidbody2D mRigidBody;
+    public float speedX = 5f;
+
+    private void Awake()
+    {
+        mRigidBody = GetComponent<Rigidbody2D>();
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +22,8 @@ public class Mario : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float moveX = Input.GetAxis("Horizontal");
+        Vector2 movement = new Vector2(moveX * speedX, mRigidBody.velocity.y);
+        mRigidBody.velocity = movement;
     }
 }
