fd950312f9fab938ccc5f81be15e03b4786980a9
mangelcomino <128420153+mangelcomino@users.noreply.github.com>
2023-06-17 19:10:25 +0200

    movimiento

--- SuperMario/Assets/Scripts/Mario.cs
+++ SuperMario/Assets/Scripts/Mario.cs
@@ -10,13 +10,21 @@ public class Mario : MonoBehaviour
     private Rigidbody2D mRigidBody;
     private Animator mAnimator;
     private SpriteRenderer mSpriteRenderer;
+    private BoxCollider2D mBoxCollider;
     public float speedX = 5f;
+    public float JumpForce = 5f;
+    public bool isGrounded=false;
+    public LayerMask FloorLayer;
+    public float moveX;
+    public ePlayerState State=ePlayerState.Idle;
 
     private void Awake()
     {
         mRigidBody = GetComponent<Rigidbody2D>();
         mAnimator = GetComponent<Animator>();
         mSpriteRenderer = GetComponent<SpriteRenderer>();
+        mBoxCollider = GetComponent<BoxCollider2D>();
+       
     }
     // Start is called before the first frame update
     void Start()
@@ -24,17 +32,83 @@ public class Mario : MonoBehaviour
         
     }
 
+    private void CheckGround()
+    {
+        Vector2 groundCheckPosition = (Vector2) this.transform.position + this.mBoxCollider.offset;
+        Collider2D[] colliders = Physics2D.OverlapBoxAll(groundCheckPosition, this.mBoxCollider.size, 0f);
+        isGrounded = false;
+        foreach (var coll in colliders) 
+        {
+            if (coll.tag == ("Player"))
+                continue;
+            isGrounded=true;
+            break;
+        }
+        
+
+    }  
+
+    private void CheckInput()
+    {
+        moveX = Input.GetAxis("Horizontal");
+        if (moveX != 0f)
+            Run(moveX);
+        else Stop();
+
+        if (moveX < 0)
+            mSpriteRenderer.flipX = true;
+        else mSpriteRenderer.flipX = false;
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) && isGrounded)
+            Jump();
+        
+    }
+
+    private void Run(float moveX)
+    {
+        State = ePlayerState.Run;
+        Vector2 movement = new Vector2(moveX * speedX, mRigidBody.velocity.y);
+        mRigidBody.velocity = movement;
+    }
+
+    private void Stop()
+    {
+        State = ePlayerState.Idle;
+    }
+    private void Jump()
+    {
+        State=ePlayerState.Jump;
+        mRigidBody.AddForce(new Vector2(0f, JumpForce));
+    }
+
+    private void CheckState()
+    {
+        if (!isGrounded)
+        {
+            mAnimator.Play("MarioJump");
+            return;
+        }
+        switch (State)
+        {
+            case ePlayerState.Idle:
+                mAnimator.Play("MarioIdle");
+                break;
+            case ePlayerState.Run:
+                mAnimator.Play("MarioRun");
+                break;
+            case ePlayerState.Jump:
+                mAnimator.Play("MarioJump");
+                break;
+        }
+    }
+
+
     // Update is called once per frame
     void Update()
     {
-        float moveX = Input.GetAxis("Horizontal");
-        Vector2 movement = new Vector2(moveX * speedX, mRigidBody.velocity.y);
-        mRigidBody.velocity = movement;
-        if (moveX != 0)
-            mAnimator.Play("MarioRun");
-        else mAnimator.Play("MarioIdle");
-        if (moveX<0)
-            mSpriteRenderer.flipX = true;
-        else mSpriteRenderer.flipX = false;
+        CheckGround();
+        CheckInput();
+        CheckState();
+        //isGrounded=Physics2D.OverlapCircle()
     }
 }
